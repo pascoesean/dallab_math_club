66c1c223317ef5a6cfb67e70704c79085b5b6298
pascoesean <seanpascoe2024@u.northwestern.edu>
2025-06-24 12:53:01 -0400

    update pca code

--- L01-PCA/pca.py
+++ L01-PCA/pca.py
@@ -42,3 +42,25 @@ palmer_with_pcs = pd.merge(palmer, scores_df, left_index = True, right_index = T
 plt.figure()
 sns.scatterplot(data = palmer_with_pcs, x = 'PC_1', y = 'PC_2', hue = 'species', style = 'sex')
 
+
+## TOY DATA ----
+# we can also do the same process on our toy data
+
+toys = np.array([[1, -1], [-1, 1], [2, -2], [-2, 2]])
+
+U, s, Vh = svd(toys)
+
+scores = U[:, 0:(len(s))] @ np.diag(s) # here, scores @ Vh = scaled_palmer bc lossless bc nrows > ncols = npcs
+eigenvals = (s**2) / (len(s) - 1)
+
+# here the eigenvales are (basically) the same as what you get on paper
+# so is my U1 vector
+# my signs are flipped for V matrix? but ig that doesn't matter (?)
+
+pca = PCA()
+
+pca.fit(toys)
+
+pca.singular_values_ # this is our s matrix! whoopie
+pca.explained_variance_ # not sure how we got this. lol
+pca.components_
